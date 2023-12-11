c1b1afb8e060f423e839a9607a26ca7bb93a1130
Yousef Soliman <yousef.soliman02@gmail.com>
2023-12-11 10:48:53 +0200

    zsh & tmux & lazyvim

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,13 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    format = {
+      timeout_ms = 10000,
+    },
+    formatters_by_ft = {
+      xml = { "xmlformat" },
+      html = { "prettier" },
+      htmldjango = { "djlint" },
+    },
+  },
+}
